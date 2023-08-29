25d122aec8cc2d9a228667e9dc1a7e0ae8641f6f
chendli <chendli@cisco.com>
2023-08-28 17:31:25 -0700

    add repo structure and deployment

---
+++ pages/1_Repo_Structure.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from utils.utils import page_config
+
+page_config(page_title='Repo')
+
+st.title('Repo Structure')
---
+++ pages/7_Deployment.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from utils.utils import page_config
+
+page_config(page_title='Deployment')
+
+st.title('Deploy the App')
+
+st.markdown('')
+
+st.markdown('### Streamlit Cloud')
+st.markdown(
+    '- [streamlit cloud deployment](https://docs.streamlit.io/streamlit-community-cloud/deploy-your-app)')
+st.markdown('### CAE')
+st.markdown(
+    "- [Vivek's Knowledge Sharing](https://wiki.cisco.com/display/DANALYTICS/Knowledge+Sharing+Session+Resource+Center) ")
+st.markdown('### GCP')
